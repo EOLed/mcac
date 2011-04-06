4e6a2c834b4c1badb55efd4c8d229c1bf721b1b0
Amos Chan <amos.chan+github@chapps.org>
2011-04-05 21:25:32 -0400

    Group view for MCACo

--- www/app/config/config.php
+++ www/app/config/config.php
@@ -3,3 +3,6 @@ $config["User"]["passwordMinLength"] = 6;
 $config["User"]["passwordMaxLength"] = 30;
 
 $config["General"]["cdn"] = true;
+
+$config["Banner"]["defaultWidth"] = 700;
+$config["Banner"]["panelWidth"] = 460;
