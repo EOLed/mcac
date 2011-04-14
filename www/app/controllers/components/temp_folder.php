22d60b281af70ddd0d3f93c74466072454807c64
Amos Chan <amos.chan+github@chapps.org>
2011-04-13 21:56:10 -0400

    Remove reduntant slash in temporary dir

--- www/app/controllers/components/temp_folder.php
+++ www/app/controllers/components/temp_folder.php
@@ -1,7 +1,7 @@
 <?php
 class TempFolderComponent extends Object {
     function mkdir() {
-        $folder = TMP . "/" . date("Ymd");
+        $folder = TMP . date("Ymd");
 
         if (!file_exists($folder))
             mkdir($folder, 0777, true);
