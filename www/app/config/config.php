e5947f27e02a2a6dcd133c631da58e7fdd8acb23
Amos Chan <amos.chan+github@chapps.org>
2011-06-03 16:31:18 -0400

    Replaced actual config with example config file

--- www/app/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$config["User"]["passwordMinLength"] = 6;
-$config["User"]["passwordMaxLength"] = 30;
-
-$config["General"]["cdn"] = true;
-
-$config["Banner"]["defaultWidth"] = 700;
-$config["Banner"]["panelWidth"] = 460;
